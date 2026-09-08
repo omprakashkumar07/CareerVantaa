297298377bbb82ac97e93420a7bc50a4e8c15764
Om Prakash Kumar <examswar3@gmail.com>
2026-09-08 18:32:25 +0530

    Update live payment links with callback configurations

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const PAYMENT_LINKS = {
-  starter: "https://rzp.io/rzp/umhOvEW",
-  accelerator: "https://rzp.io/rzp/SwOoPNKt",
-  launch: "https://rzp.io/rzp/OvJizkN"
+  starter: "https://rzp.io/rzp/uGJjT3s",
+  accelerator: "https://rzp.io/rzp/8eolAA",
+  launch: "https://rzp.io/rzp/5i0jdJF"
 };
 
 export const EDGE_FUNCTIONS_URL = import.meta.env.VITE_SUPABASE_FUNCTIONS_URL || 'http://localhost:54321/functions/v1';
