02e9a51a92d2a3bec1f1b0270d1482cd39a91b83
Om Prakash Kumar <examswar3@gmail.com>
2026-09-08 17:19:07 +0530

    Configure live Razorpay payment links

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const PAYMENT_LINKS = {
-  starter: "",
-  accelerator: "",
-  launch: ""
+  starter: "https://rzp.io/rzp/umhOvEW",
+  accelerator: "https://rzp.io/rzp/SwOoPNKt",
+  launch: "https://rzp.io/rzp/OvJizkN"
 };
 
 export const EDGE_FUNCTIONS_URL = import.meta.env.VITE_SUPABASE_FUNCTIONS_URL || 'http://localhost:54321/functions/v1';
